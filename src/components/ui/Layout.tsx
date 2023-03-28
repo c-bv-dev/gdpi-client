610431a0244769d1984c7461cea8e9a8f8f690cd
Xavier64 <xavierpincon@gmail.com>
2023-03-28 14:27:33 +0200

    ok

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
     return (
         <div className='h-screen w-screen flex flex-row overflow-hidden '>
             <Navbar />
-            <main className='flex-1 h-full overflow-y-auto border-2 border-red-500'>
+            <main className='flex-1 h-full overflow-y-auto '>
                 <Outlet />
             </main>
         </div>
